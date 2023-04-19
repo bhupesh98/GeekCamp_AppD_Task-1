1de423aac3e5c6d83b06931290b2dea5d5b28028
Bhupesh Dewangan <bhupeshdewangan20@gmail.com>
2023-04-19 22:49:36 +0530

    App Files Added

--- app/src/main/java/com/example/lugares/MainActivity.kt
+++ app/src/main/java/com/example/lugares/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.lugares
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatButton
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 
 class MainActivity : AppCompatActivity() {
     public
@@ -11,10 +14,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val button = findViewById<Button>(R.id.button)
+        val button = findViewById<AppCompatButton>(R.id.button)
+        val rollNumber  = findViewById<EditText>(R.id.rollNumber)
         button.setOnClickListener {
-            startActivity(Intent(this@MainActivity, MainActivity2::class.java))
-            finish()
+            if (rollNumber.text.toString().isEmpty()) {
+                Toast.makeText(this,"Enter your roll number!",LENGTH_SHORT).show()
+            } else {
+                startActivity(Intent(this@MainActivity, MainActivity2::class.java))
+                finish()
+            }
         }
     }
 }
